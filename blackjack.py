b9f1e99dd2c46547b36f6b80ea236ffd51a9b01e
Steffenhagen-N <nicholassteffenhagen@gmail.com>
2024-04-11 15:36:57 -0700

    added card and deck objects

--- blackjack.py
+++ blackjack.py
@@ -1,2 +1,4 @@
+from main import Card, Card_Game, Deck_of_Cards
+
 blackjack = Card_Game
 #try to get to 21 without going over
--- main.py
+++ main.py
@@ -1,21 +1,57 @@
-suits = ["clubs", "diamonds", "hearts", "spades"]
-ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
 
 class Card:
-    pass
-    add
-    greater than
-    less than
-    equal
-    equal + suits
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.__suit_index = suits.index(suit)
+        self.__rank_index = ranks.index(rank)
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+    # add
+    # greater than
+    # less than
+    # equal
+    # equal + suits
+    # __stng__ description
 
 class Deck_of_Cards:
-    pass
-    shuffle
-    draw Card
+    def __init__(self, low = True):
+        self.__deck = []
+        new_ranks = []
+        
+        for r in ranks:
+            new_ranks.append(r)
+        
+        if low:
+            pass
+        if not low:
+            new_ranks.remove("Ace")
+            new_ranks.append("Ace")
+
+        for suit in suits:
+            for rank in new_ranks:
+                self.__deck.append(Card(suit, rank))
+
+    def shuffle(self):
+        pass
+
+    def draw(self):
+        return self.__deck.pop()
 
 class Card_Game:
-    create Deck_of_Cards
-    create player hand
-    create dealer hand
+    def __init__(self, low = True):
+        self.deck = Deck_of_Cards(low)
     
+    # create Deck_of_Cards
+    # create player hand
+    # create dealer hand
+
+def main():
+    my_deck = Deck_of_Cards()
+    print(my_deck.draw())
+
+main()
