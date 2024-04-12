ab0087b674d444d3bb8beace36993977c0d1a804
Steffenhagen-N <nicholassteffenhagen@gmail.com>
2024-04-11 20:56:34 -0700

    added shuffle, draw, and getter functionality to player and deck classes

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class Card:
         self.__suit_index = suits.index(suit)
         self.__rank_index = ranks.index(rank)
 
+        # sets rank value to make math easier
         if type(self.rank) is int:
             self.__rank_value = self.rank
         if type(self.rank) is str:
@@ -22,6 +23,9 @@ class Card:
     def __repr__(self):
         return f"{self.rank} of {self.suit}"
 
+    # aces will always have a rank value of 1
+    # specific programs have to compensate if aces are 
+    # supposed to be worth 11
     def __add__(self, card):
         return self.__rank_value + card.__rank_value
     
@@ -31,25 +35,34 @@ class Card:
     def __gt__(self, card):
         if self.rank == card.rank:
             return self.__suit_index > card.__suit_index
-        # figure out how to account for aces
-        # right now aces are low, make them high
+        if self.rank == "Ace":
+            return True
+        if card.rank == "Ace":
+            return False
+        return self.__rank_index > card.__rank_index
 
     def __lt__(self, card):
-        pass
-        # same issue as __gt__
+        if self.rank == card.rank:
+            return self.__suit_index < card.__suit_index
+        if self.rank == "Ace":
+            return False
+        if card.rank == "Ace":
+            return True
+        return self.__rank_index < card.__rank_index
 
     def __eq__(self, card):
         return self.suit ==card.suit and self.rank == card.rank
 
-
+# think about adding a graveyard and a top to yard and reshuffle feature
 class Deck_of_Cards:
     def __init__(self, low = True):
         self.__deck = []
+
         new_ranks = []
-        
         for r in ranks:
             new_ranks.append(r)
         
+        # changes low aces to high aces
         if not low:
             new_ranks.remove("Ace")
             new_ranks.append("Ace")
@@ -67,20 +80,33 @@ class Deck_of_Cards:
     def shuffle(self):
         random.shuffle(self.__deck)
 
-    def draw(self, hand, value = 1):
-        for i in range(value):
-            hand.append(self.__deck.pop())
-
+    def draw(self):
+        return self.__deck.pop()
+    
+# add discard function
 class Player:
-    pass
-    #make hand
-    #draw function
+    def __init__(self, name):
+        self.hand = []
+        self.__name = name
+
+    def draw(self, deck, value = 1):
+        for i in range(value):
+            self.hand.append(deck.draw())
+
+    def get_name(self):
+        return self.__name
+    
+    def get_hand(self):
+        temp_hand = []
+        for card in self.hand:
+            temp_hand.append(f"{card.rank} of {card.suit}")
+        return ", " .join(temp_hand)
         
 class Card_Game:
     def __init__(self, low = True):
         self.deck = Deck_of_Cards(low)
-        player = Player
-        dealer = Player
+        self.player = Player("Player 1")
+        self.dealer = Player("The Dealer")
 
     def play(self):
         return ("There is nothing to play...")
@@ -88,6 +114,15 @@ class Card_Game:
 def main():
     my_game = Card_Game()
     my_game.deck.shuffle()
+    print("--- Deck ---")
+    print("")
     print(my_game.deck)
+    print("")
+    my_game.player.draw(my_game.deck, 5)
+    print(f"{my_game.player.get_name()} hand:")
+    print(my_game.player.get_hand())
+    print("")
+    print(f"Is {my_game.player.hand[0]} greater than {my_game.player.hand[1]}")
+    print(my_game.player.hand[0] > my_game.player.hand[0])
 
 main()
