f4fedf5a68175deda5c629aa970d165f598e5153
Steffenhagen-N <nicholassteffenhagen@gmail.com>
2024-04-11 17:29:34 -0700

    added functionality to card, deck, and game classes, added player class

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,4 @@
-from main import Card, Card_Game, Deck_of_Cards
+from main import Card, Card_Game, Deck_of_Cards, Player
 
 blackjack = Card_Game
 #try to get to 21 without going over
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
 
@@ -5,18 +7,40 @@ class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+        self.__rank_value = 0
         self.__suit_index = suits.index(suit)
         self.__rank_index = ranks.index(rank)
 
-    def __str__(self):
+        if type(self.rank) is int:
+            self.__rank_value = self.rank
+        if type(self.rank) is str:
+            if self.rank == "Ace":
+                self.__rank_value = 1
+            if self.rank == "Jack" or "Queen" or "King":
+                self.__rank_value = 10
+
+    def __repr__(self):
         return f"{self.rank} of {self.suit}"
 
-    # add
-    # greater than
-    # less than
-    # equal
-    # equal + suits
-    # __stng__ description
+    def __add__(self, card):
+        return self.__rank_value + card.__rank_value
+    
+    def __sub__(self, card):
+        return self.__rank_value - card.__rank_value
+
+    def __gt__(self, card):
+        if self.rank == card.rank:
+            return self.__suit_index > card.__suit_index
+        # figure out how to account for aces
+        # right now aces are low, make them high
+
+    def __lt__(self, card):
+        pass
+        # same issue as __gt__
+
+    def __eq__(self, card):
+        return self.suit ==card.suit and self.rank == card.rank
+
 
 class Deck_of_Cards:
     def __init__(self, low = True):
@@ -26,8 +50,6 @@ class Deck_of_Cards:
         for r in ranks:
             new_ranks.append(r)
         
-        if low:
-            pass
         if not low:
             new_ranks.remove("Ace")
             new_ranks.append("Ace")
@@ -36,22 +58,36 @@ class Deck_of_Cards:
             for rank in new_ranks:
                 self.__deck.append(Card(suit, rank))
 
+    def __repr__(self):
+        deck_list = []
+        for card in self.__deck:
+            deck_list.append(f"{card.rank} of {card.suit}")
+        return ", " .join(deck_list)
+
     def shuffle(self):
-        pass
+        random.shuffle(self.__deck)
 
-    def draw(self):
-        return self.__deck.pop()
+    def draw(self, hand, value = 1):
+        for i in range(value):
+            hand.append(self.__deck.pop())
 
+class Player:
+    pass
+    #make hand
+    #draw function
+        
 class Card_Game:
     def __init__(self, low = True):
         self.deck = Deck_of_Cards(low)
-    
-    # create Deck_of_Cards
-    # create player hand
-    # create dealer hand
+        player = Player
+        dealer = Player
+
+    def play(self):
+        return ("There is nothing to play...")
 
 def main():
-    my_deck = Deck_of_Cards()
-    print(my_deck.draw())
+    my_game = Card_Game()
+    my_game.deck.shuffle()
+    print(my_game.deck)
 
 main()
